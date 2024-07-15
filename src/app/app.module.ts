5b51996d1bcc95a5d9650cc960204a38158dd0f6
whereismyname7 <kalshmrani70@hotmail.com>
2024-07-15 13:38:17 +0300

    Skeleton-created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/layouts/header/header.component';
+import { FooterComponent } from './components/layouts/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { PieGridComponent } from './components/charts/pie-grid/pie-grid.component';
+import { AdvancedPieComponent } from './components/charts/advanced-pie/advanced-pie.component';
+import { StackedBarComponent } from './components/charts/stacked-bar/stacked-bar.component';
+import { GroupedBarComponent } from './components/charts/grouped-bar/grouped-bar.component';
+import { ButtonComponent } from './components/layouts/button/button.component';
+import { BreadcrumbsComponent } from './components/layouts/breadcrumbs/breadcrumbs.component';
+import { MessageComponent } from './components/layouts/message/message.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    PieGridComponent,
+    AdvancedPieComponent,
+    StackedBarComponent,
+    GroupedBarComponent,
+    ButtonComponent,
+    BreadcrumbsComponent,
+    MessageComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/charts/grouped-bar/grouped-bar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-grouped-bar',
+  templateUrl: './grouped-bar.component.html',
+  styleUrl: './grouped-bar.component.css'
+})
+export class GroupedBarComponent {
+
+}
