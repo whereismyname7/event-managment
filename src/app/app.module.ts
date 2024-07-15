ead9029bf8a10ce568d63b4f327949f00bb823ec
whereismyname7 <kalshmrani70@hotmail.com>
2024-07-15 17:37:18 +0300

    file structure updated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/layouts/header/header.component';
 import { FooterComponent } from './components/layouts/footer/footer.component';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './pages/login/login.component';
 import { PieGridComponent } from './components/charts/pie-grid/pie-grid.component';
 import { AdvancedPieComponent } from './components/charts/advanced-pie/advanced-pie.component';
 import { StackedBarComponent } from './components/charts/stacked-bar/stacked-bar.component';
@@ -13,7 +13,7 @@ import { GroupedBarComponent } from './components/charts/grouped-bar/grouped-bar
 import { ButtonComponent } from './components/layouts/button/button.component';
 import { BreadcrumbsComponent } from './components/layouts/breadcrumbs/breadcrumbs.component';
 import { MessageComponent } from './components/layouts/message/message.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
