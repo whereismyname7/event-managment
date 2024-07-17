99f0cc972dde350865c33d70f4173846fcf340f2
bayanbamb <bayanbamb@yahoo.com>
2024-07-17 12:59:02 +0300

    Fix

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators } from "@angular/forms";
 import { Router } from '@angular/router';
+import { AppRoutes } from '../../app.constants';
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
+
 export class LoginComponent {
+  constructor(private router: Router) {}
+
   submitted = false;
 
   loginForm = new FormGroup({
@@ -24,7 +29,7 @@ export class LoginComponent {
     this.submitted = true;
     if (this.loginForm.valid) {
       this.submitted = false;
-      // this.router.navigate([ROUTES.DASHBOARD]);
+      this.router.navigate([AppRoutes.DASHBOARD]);
     }
   }
 
