b198a698fe4799c8a1d0c56910a04be6cdf6f4a5
bayanbamb <bayanbamb@yahoo.com>
2024-07-17 17:37:21 +0300

    Created pie grid chart

--- src/app/components/charts/dummyData.ts
+++ src/app/components/charts/dummyData.ts
@@ -1,334 +1,352 @@
 export var totalEvents = [
-    {
-      "name": "1",
-      "series": [
-        {
-          "name": "عن بعد",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "حضورية",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "2",
-      "series": [
-        {
-          "name": "عن بعد",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "حضورية",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "3",
-      "series": [
-        {
-          "name": "عن بعد",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "حضورية",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "4",
-      "series": [
-        {
-          "name": "عن بعد",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "حضورية",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "5",
-      "series": [
-        {
-          "name": "عن بعد",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "حضورية",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "6",
-      "series": [
-        {
-          "name": "عن بعد",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "حضورية",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "7",
-      "series": [
-        {
-          "name": "عن بعد",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "حضورية",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "8",
-      "series": [
-        {
-          "name": "عن بعد",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "حضورية",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "9",
-      "series": [
-        {
-          "name": "عن بعد",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "حضورية",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "10",
-      "series": [
-        {
-          "name": "عن بعد",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "حضورية",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "11",
-      "series": [
-        {
-          "name": "عن بعد",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "حضورية",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "12",
-      "series": [
-        {
-          "name": "عن بعد",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "حضورية",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    }
-  ];
-  
+  {
+    "name": "1",
+    "series": [
+      {
+        "name": "عن بعد",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "حضورية",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "2",
+    "series": [
+      {
+        "name": "عن بعد",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "حضورية",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "3",
+    "series": [
+      {
+        "name": "عن بعد",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "حضورية",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "4",
+    "series": [
+      {
+        "name": "عن بعد",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "حضورية",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "5",
+    "series": [
+      {
+        "name": "عن بعد",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "حضورية",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "6",
+    "series": [
+      {
+        "name": "عن بعد",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "حضورية",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "7",
+    "series": [
+      {
+        "name": "عن بعد",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "حضورية",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "8",
+    "series": [
+      {
+        "name": "عن بعد",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "حضورية",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "9",
+    "series": [
+      {
+        "name": "عن بعد",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "حضورية",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "10",
+    "series": [
+      {
+        "name": "عن بعد",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "حضورية",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "11",
+    "series": [
+      {
+        "name": "عن بعد",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "حضورية",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "12",
+    "series": [
+      {
+        "name": "عن بعد",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "حضورية",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  }
+];
 
-  export var totalAttendance = [
-    {
-      "name": "1",
-      "series": [
-        {
-          "name": "ذكر",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "أنثى",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "2",
-      "series": [
-        {
-          "name": "ذكر",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "أنثى",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "3",
-      "series": [
-        {
-          "name": "ذكر",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "أنثى",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "4",
-      "series": [
-        {
-          "name": "ذكر",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "أنثى",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "5",
-      "series": [
-        {
-          "name": "ذكر",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "أنثى",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "6",
-      "series": [
-        {
-          "name": "ذكر",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "أنثى",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "7",
-      "series": [
-        {
-          "name": "ذكر",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "أنثى",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "8",
-      "series": [
-        {
-          "name": "ذكر",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "أنثى",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "9",
-      "series": [
-        {
-          "name": "ذكر",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "أنثى",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "10",
-      "series": [
-        {
-          "name": "ذكر",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "أنثى",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "11",
-      "series": [
-        {
-          "name": "ذكر",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "أنثى",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
-    },
-    {
-      "name": "12",
-      "series": [
-        {
-          "name": "ذكر",
-          "value": Math.floor(Math.random() * 15001)
-        },
-        {
-          "name": "أنثى",
-          "value": Math.floor(Math.random() * 15001)
-        }
-      ]
+
+export var totalAttendance = [
+  {
+    "name": "1",
+    "series": [
+      {
+        "name": "ذكر",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "أنثى",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "2",
+    "series": [
+      {
+        "name": "ذكر",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "أنثى",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "3",
+    "series": [
+      {
+        "name": "ذكر",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "أنثى",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "4",
+    "series": [
+      {
+        "name": "ذكر",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "أنثى",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "5",
+    "series": [
+      {
+        "name": "ذكر",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "أنثى",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "6",
+    "series": [
+      {
+        "name": "ذكر",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "أنثى",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "7",
+    "series": [
+      {
+        "name": "ذكر",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "أنثى",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "8",
+    "series": [
+      {
+        "name": "ذكر",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "أنثى",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "9",
+    "series": [
+      {
+        "name": "ذكر",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "أنثى",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "10",
+    "series": [
+      {
+        "name": "ذكر",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "أنثى",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "11",
+    "series": [
+      {
+        "name": "ذكر",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "أنثى",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  },
+  {
+    "name": "12",
+    "series": [
+      {
+        "name": "ذكر",
+        "value": Math.floor(Math.random() * 15001)
+      },
+      {
+        "name": "أنثى",
+        "value": Math.floor(Math.random() * 15001)
+      }
+    ]
+  }
+];
+export var eventTypes = [
+  {
+    "name": "حضورية",
+    "value": Math.floor(Math.random() * 50001),
+    "extra": {
+      "code": "ons"
     }
-  ];
-  export var eventTypes = [
-    {
-      "name": "حضورية",
-      "value": Math.floor(Math.random() * 50001),
-      "extra": {
-        "code": "ons"
-      }
-    },
-    {
-      "name": "عن بعد",
-      "value": Math.floor(Math.random() * 50001),
-      "extra": {
-        "code": "onl"
-      }
-    },
-  ];
+  },
+  {
+    "name": "عن بعد",
+    "value": Math.floor(Math.random() * 50001),
+    "extra": {
+      "code": "onl"
+    }
+  },
+];
+export var eventCategories = [
+  {
+    "name": "ترفيهية",
+    "value": 5000
+  },
+  {
+    "name": "تعليمية",
+    "value": 4500
+  },
+  {
+    "name": "مهنية",
+    "value": 3000
+  },
+  {
+    "name": "أخرى",
+    "value": 4000
+  },
+]
