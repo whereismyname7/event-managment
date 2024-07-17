117396554cabae73413675ee4b1ac7e2526c4e6b
whereismyname7 <kalshmrani70@hotmail.com>
2024-07-17 15:18:01 +0300

    fix: rtl in header and footer + header and footer are hidden in login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'event-managment';
+
+  //shows header and footer
+  show = true;
+
+  constructor(private router: Router) {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Check if current route is the login page
+        if (event.url === '/') {
+          this.show = false;
+        } else {
+          this.show = true;
+        }
+      }
+    });
+  }
+
 }
