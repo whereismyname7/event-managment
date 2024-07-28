e4c2e9da7db7bb9d1bac780ee1c48580cd4003ff
whereismyname7 <kalshmrani70@hotmail.com>
2024-07-28 12:42:48 +0300

    constructor removed

--- src/app/components/events/events-card-list/events-card-list.component.ts
+++ src/app/components/events/events-card-list/events-card-list.component.ts
@@ -11,33 +11,12 @@ export class EventsCardListComponent {
 
   allEvents = events;
 
-  constructor(private translateService: TranslateService) {
-    this.translateService.addLangs(['en', 'ar']);
-    this.translateService.setDefaultLang('ar');
-
-    const browserLang = this.translateService.getBrowserLang();
-    this.translateService.use(browserLang && browserLang.match(/en|ar/) ? browserLang : 'en');
-    this.translateEvents();
-    
-  }
-
-  translateEvents() {
-    
-    this.allEvents = events.map(event => ({
-      ...event,
-        name: this.translateService.instant(event.name)
-      }));
-
-    Object.assign(this.allEvents);
-  }
 
   @Output() eventsCount = new EventEmitter<number>();
 
   ngOnInit() {
     this.emitEventsCount();
-    this.translateService.onLangChange.subscribe(() => {
-      this.translateEvents();
-    });
+   
   }
 
   emitEventsCount() {
