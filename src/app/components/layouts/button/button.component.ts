fec5f1b00b15f7880b3b0ca169ecb28917af4bd0
MohammadWS <mohammadwalomair@gmail.com>
2024-07-30 13:38:09 +0300

    feat: customize button component

--- src/app/components/layouts/button/button.component.ts
+++ src/app/components/layouts/button/button.component.ts
@@ -3,9 +3,25 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
-  styleUrl: './button.component.css'
+  styleUrl: './button.component.css',
+  styles: [`
+    .outline{  
+    border: 2px solid #264653;
+    background-color: white;
+    color: #264653;
+    }
+    .fill{
+    background: #264653;
+    color: #FFFFFF;
+    }
+    `
+  ]
 })
 export class ButtonComponent {
   @Input() text: any;
+  @Input() customClasses: string | string[] = '';
+  @Input() buttonType: string = 'button';
+
+
 
 }
